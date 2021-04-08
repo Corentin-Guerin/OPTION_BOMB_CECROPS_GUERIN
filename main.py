87e9721a364f32e2761695176b7d17285a0bdbda
Corentin-Guerin <74669697+Corentin-Guerin@users.noreply.github.com>
2021-04-08 08:57:23 +0200

    Fenetre

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((640,480))
 
+#creation de la surface dans la fenetre python
+#set_mode = cree une surface avec en parametre la resolution de fenetre 
+taillescreen = (640,480)
+screen = pygame.display.set_mode(taillescreen)
+
+#ferme la fenetre avec la croix en haut a droite  
 launched = True
 while launched:
     for event in pygame.event.get():
